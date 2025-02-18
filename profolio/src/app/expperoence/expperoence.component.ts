cfffb6544d6e1b06ef1b839ca6099b22c5b1e12e
Jodyhany <jody.hany.511@gmail.com>
2025-02-18 17:19:07 +0200

    Add files via upload

---
+++ profolio/src/app/expperoence/expperoence.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expperoence',
+  templateUrl: './expperoence.component.html',
+  styleUrls: ['./expperoence.component.css']
+})
+export class ExpperoenceComponent {
+
+}
